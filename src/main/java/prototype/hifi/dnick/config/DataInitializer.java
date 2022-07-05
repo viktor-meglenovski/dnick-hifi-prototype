7382ea9ce99be6a2d554a3a81f23beb76161bde1
Viktor Meglenovski <viktor.meglenovski@gmail.com>
2022-07-05 15:51:25 +0200

    Static files

--- src/main/java/prototype/hifi/dnick/config/DataInitializer.java
+++ src/main/java/prototype/hifi/dnick/config/DataInitializer.java
@@ -27,6 +27,7 @@ public class DataInitializer {
     private final TestResultService testResultService;
     private final BadgeService badgeService;
     private final QuestionRepository questionRepository;
+
     public DataInitializer(TopicRepository topicRepository, TopicService topicService, UserService userService, BadgeRepository badgeRepository, TestResultService testResultService, BadgeService badgeService, QuestionRepository questionRepository) {
         this.topicRepository = topicRepository;
         this.topicService = topicService;
@@ -39,14 +40,14 @@ public class DataInitializer {
 
     @PostConstruct
     public void initData() throws IOException {
-        Topic skrshenici=new Topic("Skrshenici","Скршеници","./images/broken_bone.JPG", "https://www.youtube.com/embed/bqJNshguoTY", "/pdf/скршеници.pdf");
-        Topic izgorenici=new Topic("Izgorenici","Изгореници", "./images/burns.JPG","https://www.youtube.com/embed/IOtnGl_9-qw", "/pdf/изгореници.pdf");
-        Topic krvarenje=new Topic("Krvarenje","Крварење", "./images/bleeding.JPG","https://www.youtube.com/embed/BYQzwqrEUlU","/pdf/крварење.pdf");
-        Topic srcevoBelodrobno=new Topic("SrcevoBelodrobnoOzivuvanje","Срцево Белодробно оживување","./images/heart_lungs.JPG","https://www.youtube.com/embed/hHyVQiWFXzg","/pdf/срцево-белодробно-оживување.pdf");
-        Topic zadusuvanje=new Topic("Zadushuvanje","Задушување","./images/choking.JPG","https://www.youtube.com/embed/6E9AXXRdkfE","/pdf/задушување.pdf");
-        Topic mozocenUdar=new Topic("MozocenUdar","Мозочен удар","./images/stroke.JPG","https://www.youtube.com/embed/PCNTMIcOMpE","/pdf/мозочен-удар.pdf");
-        Topic srcevUdar=new Topic("SrcevUdar","Срцев удар", "./images/heart_attack.JPG","https://www.youtube.com/embed/1qje-XlNowI","/pdf/срцев-удар.pdf");
-        Topic smrznuvanje=new Topic("Smrznuvanje","Смрзнување","./images/hypothermia.JPG","https://www.youtube.com/embed/qlNKCSC2s5w","/pdf/смрзнување.pdf");
+        Topic skrshenici = new Topic("Skrshenici", "Скршеници", "./images/broken_bone.JPG", "https://www.youtube.com/embed/bqJNshguoTY", "/pdf/скршеници.pdf");
+        Topic izgorenici = new Topic("Izgorenici", "Изгореници", "./images/burns.JPG", "https://www.youtube.com/embed/IOtnGl_9-qw", "/pdf/изгореници.pdf");
+        Topic krvarenje = new Topic("Krvarenje", "Крварење", "./images/bleeding.JPG", "https://www.youtube.com/embed/BYQzwqrEUlU", "/pdf/крварење.pdf");
+        Topic srcevoBelodrobno = new Topic("SrcevoBelodrobnoOzivuvanje", "Срцево Белодробно оживување", "./images/heart_lungs.JPG", "https://www.youtube.com/embed/hHyVQiWFXzg", "/pdf/срцево-белодробно-оживување.pdf");
+        Topic zadusuvanje = new Topic("Zadushuvanje", "Задушување", "./images/choking.JPG", "https://www.youtube.com/embed/6E9AXXRdkfE", "/pdf/задушување.pdf");
+        Topic mozocenUdar = new Topic("MozocenUdar", "Мозочен удар", "./images/stroke.JPG", "https://www.youtube.com/embed/PCNTMIcOMpE", "/pdf/мозочен-удар.pdf");
+        Topic srcevUdar = new Topic("SrcevUdar", "Срцев удар", "./images/heart_attack.JPG", "https://www.youtube.com/embed/1qje-XlNowI", "/pdf/срцев-удар.pdf");
+        Topic smrznuvanje = new Topic("Smrznuvanje", "Смрзнување", "./images/hypothermia.JPG", "https://www.youtube.com/embed/qlNKCSC2s5w", "/pdf/смрзнување.pdf");
 
 
         topicRepository.save(krvarenje);
@@ -59,11 +60,11 @@ public class DataInitializer {
         topicRepository.save(smrznuvanje);
 
 
-        Badge tests1=new Badge(Badges.TESTS_1,"Направи 1 тест!");
-        Badge tests3=new Badge(Badges.TESTS_3,"Направи 3 тестови!");
-        Badge tests5=new Badge(Badges.TESTS_5,"Направи 5 тестови!");
-        Badge pointsOver90=new Badge(Badges.SCORE_OVER_90,"Освој над 90 поени!");
-        Badge points100=new Badge(Badges.SCORE_100,"Освој 100 поени!");
+        Badge tests1 = new Badge(Badges.TESTS_1, "Направи 1 тест!");
+        Badge tests3 = new Badge(Badges.TESTS_3, "Направи 3 тестови!");
+        Badge tests5 = new Badge(Badges.TESTS_5, "Направи 5 тестови!");
+        Badge pointsOver90 = new Badge(Badges.SCORE_OVER_90, "Освој над 90 поени!");
+        Badge points100 = new Badge(Badges.SCORE_100, "Освој 100 поени!");
 
         badgeRepository.save(tests1);
         badgeRepository.save(tests3);
@@ -72,37 +73,38 @@ public class DataInitializer {
         badgeRepository.save(points100);
 
 
-        User test=userService.register("ViktorM","test","test","Viktor","Meglenovski", Role.ROLE_USER);
+        User test = userService.register("ViktorM", "test", "test", "Viktor", "Meglenovski", Role.ROLE_USER);
 
-        topicService.markAsCompleted("Skrshenici",test);
-        topicService.markAsCompleted("SrcevUdar",test);
-        topicService.markAsCompleted("Krvarenje",test);
+        topicService.markAsCompleted("Skrshenici", test);
+        topicService.markAsCompleted("SrcevUdar", test);
+        topicService.markAsCompleted("Krvarenje", test);
 
-        TestResult testResult1= testResultService.saveNewResult(test,92);
-        TestResult testResult2= testResultService.saveNewResult(test,70);
-        TestResult testResult3= testResultService.saveNewResult(test,15);
+        TestResult testResult1 = testResultService.saveNewResult(test, 92);
+        TestResult testResult2 = testResultService.saveNewResult(test, 70);
+        TestResult testResult3 = testResultService.saveNewResult(test, 15);
         badgeService.checkForAll(testResult1);
         badgeService.checkForAll(testResult2);
         badgeService.checkForAll(testResult3);
 
         loadQuestions();
     }
-    private void loadQuestions() throws IOException {
-        File questions=new File(".\\src\\main\\resources\\static\\questions.txt");
-        BufferedReader reader=new BufferedReader(new FileReader(questions));
-        String line = reader.readLine();
-        while (line != null && line!="") {
 
-            String[] parts=line.split(";");
-            List<String> answers=new ArrayList<>();
-            for(int i=2;i<parts.length;i++){
+    private void loadQuestions() throws IOException {
+        //File questions = new File(".\\src\\main\\resources\\static\\questions.txt");
+        InputStream inputStream = getClass().getResourceAsStream("/static/questions.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = reader.readLine();
+        while (line != null && line != "") {
+
+            String[] parts = line.split(";");
+            List<String> answers = new ArrayList<>();
+            for (int i = 2; i < parts.length; i++) {
                 answers.add(parts[i]);
             }
-            questionRepository.save(new Question(parts[0],answers,parts[1].charAt(0)));
+            questionRepository.save(new Question(parts[0], answers, parts[1].charAt(0)));
 
             line = reader.readLine();
         }
         reader.close();
-
     }
 }
